55f3a056da25ecda4d263506b8e84f20e960bd07
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2022-05-11 18:19:16 +0100

    #1 more attempts at connection

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,24 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
 
 	kit: {
+		paths: {
+			base: dev ? '' : '/crypto-lab'
+		},
+		// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
+		// For example, instead of '_app', use 'app_', 'internal', etc.
+		appDir: 'build',
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
-			fallback: null
+			fallback: null,
+			precompress: false
 		})
 	}
 };
