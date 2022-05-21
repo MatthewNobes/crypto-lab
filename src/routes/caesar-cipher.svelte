48381a6e5e8c12d60a481263e636147222b22a09
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2022-05-21 20:03:51 +0100

    #1 Setting up proper navigation

--- src/routes/caesar-cipher.svelte
+++ src/routes/caesar-cipher.svelte
@@ -1,6 +1,10 @@
+<script>
+	import Navigation from '../components/Navigation.svelte';
+</script>
+
 <svelte:head>
 	<title>Caesar Cipher</title>
 </svelte:head>
 
 <h1>Caesar Cipher</h1>
-<p>TODO...</p>
+<Navigation />
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,11 @@
+<script>
+	//import Navigation from "src/components/Navigation.svelte";
+	import Navigation from '../components/Navigation.svelte';
+</script>
+
 <svelte:head>
 	<title>Crypto Lab</title>
 </svelte:head>
 
 <h1>Hello and welcome to my site!</h1>
-<a href="/caesar-cipher">Caesar</a>
+<Navigation />
