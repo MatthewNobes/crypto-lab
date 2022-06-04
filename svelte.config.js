d00820d51cda8592a6108c72a390e264f7cbbca1
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2022-06-04 20:33:41 +0100

    #2 Setting up adapter-static properly

--- svelte.config.js
+++ svelte.config.js
@@ -8,18 +8,18 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		paths: {
-			base: dev ? '' : '/crypto-lab'
-		},
-		// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
-		// For example, instead of '_app', use 'app_', 'internal', etc.
-		appDir: 'build',
 		adapter: adapter({
+			// default options are shown
 			pages: 'build',
 			assets: 'build',
 			fallback: null,
 			precompress: false
-		})
+		}),
+
+		prerender: {
+			// This can be false if you're using a fallback (i.e. SPA mode)
+			default: true
+		}
 	}
 };
 
