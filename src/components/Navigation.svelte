f1c07bc8d9a4a025a584565049ccb986dece327d
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2022-07-02 21:35:19 +0100

    #1 Nav bar config

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -1,4 +1,8 @@
-<div>
-	<a href="/">Home</a>
-	<a href="/caesar-cipher">Caesar</a>
+<script>
+	import NavigationLink from './NavigationLink/NavigationLink.svelte';
+</script>
+
+<div class="flex flex-row bg-slate-500 w-full py-2">
+	<NavigationLink linkName="Home" linkRoute="/" />
+	<NavigationLink linkName="Caesar Cipher" linkRoute="/caesar-cipher" />
 </div>
---
+++ src/components/NavigationLink/NavigationLink.svelte
@@ -0,0 +1,14 @@
+<script>
+	export /**
+	 * @type {any}
+	 */
+	let linkName;
+	export /**
+	 * @type {string | null | undefined}
+	 */
+	let linkRoute;
+</script>
+
+<div>
+	<a href={linkRoute} class="text-white bg-slate-500 px-3">{linkName}</a>
+</div>
